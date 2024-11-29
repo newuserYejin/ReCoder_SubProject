b9c9ddf4f837e3c2a2793f086b6593da120f2d24
jin <jin200126@gmail.com>
2024-11-29 16:35:53 +0900

    test 2024.11.29

---
+++ reFactorial/src/main/java/com/ohgiraffers/refactorial/ReFactorialApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.refactorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReFactorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReFactorialApplication.class, args);
+	}
+
+}
