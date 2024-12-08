2a86021c19b69ddc791ffacc143eac1016a44f7f
jin <jin200126@gmail.com>
2024-12-08 22:43:28 +0900

    frontend ing mypage

--- reFactorial/src/main/java/com/ohgiraffers/refactorial/CommonController.java
+++ reFactorial/src/main/java/com/ohgiraffers/refactorial/CommonController.java
@@ -15,45 +15,9 @@ import java.util.Collection;
 @ControllerAdvice
 public class CommonController {
 
-//    @ModelAttribute("LoginUserInfo")
-//    public UserDTO LoginUserInfo(){
-//
-//        // 현재 인증된 사용자 정보를 가져오기
-////        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-////
-////        // AuthDetails 객체 가져오기
-////        AuthDetails authDetails = (AuthDetails) authentication.getPrincipal();
-////
-////        // AuthDetails 객체에서 사용자 정보 가져오기
-////        UserDTO user = authDetails.getUser(); // AuthDetails 내부의 UserDTO 객체
-//
-//        // 사용자 ID를 모델에 추가
-////        model.addAttribute("userId", user.getEmpId());  // 또는 user.getEmpId(), user.getUsername() 등을 사용
-////
-////        // 권한 정보 가져오기
-////        Collection<? extends GrantedAuthority> authorities = authDetails.getAuthorities();
-////
-////        // 권한 정보를 모델에 추가
-////        model.addAttribute("roles", authorities);
-////        model.addAttribute("userDTO", user);
-//
-////        System.out.println("user = " + user);
-////
-////        return user;
-//
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        System.out.println("Authentication is available: " + authentication);
-//
-//        if (authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal() instanceof String) {
-//            return new UserDTO(); // 기본값으로 빈 UserDTO 반환
-//        }
-//        AuthDetails authDetails = (AuthDetails) authentication.getPrincipal();
-//        return authDetails.getUser();
-//    }
-
     @ModelAttribute("LoginUserInfo")
     public UserDTO LoginUserInfo(HttpSession session) {
-        System.out.println("session.getAttribute(\"LoginUserInfo\") = " + session.getAttribute("LoginUserInfo"));
+//        System.out.println("session.getAttribute(\"LoginUserInfo\") = " + session.getAttribute("LoginUserInfo"));
 
         UserDTO user = (UserDTO) session.getAttribute("LoginUserInfo");
 
--- reFactorial/src/main/java/com/ohgiraffers/refactorial/config/AuthSuccessHandler.java
+++ reFactorial/src/main/java/com/ohgiraffers/refactorial/config/AuthSuccessHandler.java
@@ -15,35 +15,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.io.IOException;
 
-//@Component
-//public class AuthSuccessHandler implements AuthenticationSuccessHandler {
-//    @Override
-//    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
-//        AuthDetails authDetails = (AuthDetails) authentication.getPrincipal();
-//        UserDTO user = authDetails.getUser();
-//
-//        request.getSession().setAttribute("LoginUserInfo", user);
-//
-//        System.out.println("user = " + user);
-//        response.sendRedirect("/user");
-//    }
-//
-//    @Override
-//    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-//
-//    }
-//}
-
 @Configuration
 public class AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-
-//    public AuthSuccessHandler() {
-//        super();
-//    }
-//
-//    public AuthSuccessHandler(String defaultTargetUrl) {
-//        super(defaultTargetUrl);
-//    }
 
     @Override
     @ModelAttribute("LoginUserInfo")
