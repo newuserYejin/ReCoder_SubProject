870f196051d1c69889ac03f00cd4db9304c3e395
Yongwook <kyw9885@gmail.com>
2024-12-09 17:30:40 +0900

    [1209] board_regist

--- reFactorial/src/main/java/com/ohgiraffers/refactorial/board/model/dao/BoardMapper.java
+++ reFactorial/src/main/java/com/ohgiraffers/refactorial/board/model/dao/BoardMapper.java
@@ -1,4 +1,11 @@
 package com.ohgiraffers.refactorial.board.model.dao;
 
+import com.ohgiraffers.refactorial.board.model.dto.BoardDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface BoardMapper {
+    void post(BoardDTO board);
+
+    void boardPost(BoardDTO board);
 }
