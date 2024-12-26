c0224dc2f9a1d7acaab3de0c6e5f47618eaafe78
MyComputer <playdrew776p@gmail.com>
2024-12-26 23:12:16 +0900

    [24-12-26]

--- reFactorial/src/main/java/com/ohgiraffers/refactorial/inquiry/model/dto/InquiryDTO.java
+++ reFactorial/src/main/java/com/ohgiraffers/refactorial/inquiry/model/dto/InquiryDTO.java
@@ -2,6 +2,8 @@ package com.ohgiraffers.refactorial.inquiry.model.dto;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -12,6 +14,7 @@ public class InquiryDTO {
     private String iqrTitle;      // 문의 제목
     private String iqrDetails;    // 문의 내용
     private boolean answerStatus;  // 답변 상태
+    private LocalDateTime iqrCreationDate; // 문의 생성 날짜 및 시간
     private String empId;         // 문의자 사원번호
     private String answerDetail;   // 답변 내용 (NULL 허용)
 }
